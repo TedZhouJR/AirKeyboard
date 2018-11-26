6be4e997ae692de42832a8a9980055b2bbb867c7
Atomie CHEN <atomic_cwh@163.com>
2018-11-26 22:00:03 +0800

    按照sdk的sample.java添加了一个自定义listener

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
+import Backend.LeapMotionListener;
 import Frontend.mainWindow;
+import com.leapmotion.leap.Controller;
+
+import java.io.IOException;
 
 
 public class Main {
 
     public static void main(String[] args){
         mainWindow mainwindow = new mainWindow("Air Key Board");
+
+        // Create a sample listener and controller
+        LeapMotionListener listener = new LeapMotionListener();
+        Controller controller = new Controller();
+
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
     }
 }
